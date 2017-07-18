0df170cfe959077feb8cdb3524d133c0c1c49751
Danielle Fricke <danielle.fricke11@gmail.com>
2017-07-18 09:15:01 -0400

    added react router dom

--- src/index.js
+++ src/index.js
@@ -2,6 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import {
+  BrowserRouter as Router,
+  Route,
+  Link,
+} from 'react-router-dom'
 import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
