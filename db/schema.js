bde95b1552e4c366339c319ad9825fe06cc7a47f
Danielle Fricke <danielle.fricke11@gmail.com>
2017-07-28 17:41:44 -0400

    fixed mongoose

--- db/schema.js
+++ db/schema.js
@@ -1,5 +1,5 @@
 var mongoose = require("mongoose");
-mongoose.connect('mongodb://localhost/vacay');
+mongoose.connect('mongodb://https://vacay-app-df.herokuapp.com/vacay');
 
 var CommentSchema = new mongoose.Schema(
   {
